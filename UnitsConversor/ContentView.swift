816e004f72f9ef0fe67eff71dc509e5cba846941
Gabriel Henrique <gabhenrique0@gmail.com>
2022-02-05 20:41:33 -0300

    :recycle: Refactor: convert units to use UnitTemperature type

--- UnitsConversor/ContentView.swift
+++ UnitsConversor/ContentView.swift
@@ -10,23 +10,17 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - Properties
     @State private var temperature: Double = 20
-    @State private var temperatureUnit: String = "Celsius"
-    private let temperatureUnits: [String] = ["Celsius", "Fahrenheit", "Kelvin"]
+    @State private var temperatureUnitEntrancy: UnitTemperature = .celsius
+    private let temperatureUnits: [UnitTemperature] = [.celsius, .fahrenheit, .kelvin]
     
     // MARK: - Computed Properties
+    
     var body: some View {
         Form {
             Section {
-                TextField("Temperature", value: $temperature, format: .number)
-                    .keyboardType(.numberPad)
-            } header : {
-                Text("Temperature to convert:")
-            }
-            
-            Section {
-                Picker("Temperature Unit", selection: $temperatureUnit) {
+                Picker("Temperature Unit", selection: $temperatureUnitEntrancy) {
                     ForEach(temperatureUnits, id: \.self) {
-                        Text($0)
+                        Text($0.symbol)
                     }
                 }
                 .pickerStyle(.segmented)
@@ -35,9 +29,14 @@ struct ContentView: View {
             }
             
             Section {
-                Text(temperature, format: .number)
+                TextField("Temperature", value: $temperature, format: .number)
+                    .keyboardType(.numberPad)
             } header : {
-                Text("Temperature in \(temperatureUnit) is:")
+                Text("Temperature to convert:")
+            }
+            
+            Section {
+                Text("")
             }
         }
     }
