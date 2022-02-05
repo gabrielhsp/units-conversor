d4080c6393f0d4bd1485a069965779b86ff3cf3f
Gabriel Henrique <gabhenrique0@gmail.com>
2022-02-05 19:22:27 -0300

    Initial Commit

---
+++ UnitsConversor/UnitsConversorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsConversorApp.swift
+//  UnitsConversor
+//
+//  Created by Gabriel Pereira on 05/02/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitsConversorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
