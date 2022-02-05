d1ad80bce7a8ed6f9dbbfc510a128958c1d8b0ce
Gabriel Henrique <gabhenrique0@gmail.com>
2022-02-05 19:55:29 -0300

    :sparkles: Feat: create temperature unit base converted

--- UnitsConversor/ContentView.swift
+++ UnitsConversor/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    @State private var temperature: Double = 20
+    @State private var temperatureUnit: String = "Celsius"
+    private let temperatureUnits: [String] = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    // MARK: - Computed Properties
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Temperature", value: $temperature, format: .number)
+                    .keyboardType(.numberPad)
+            } header : {
+                Text("Temperature to convert:")
+            }
+            
+            Section {
+                Picker("Temperature Unit", selection: $temperatureUnit) {
+                    ForEach(temperatureUnits, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header : {
+                Text("What's the conversion unit?")
+            }
+            
+            Section {
+                Text(temperature, format: .number)
+            } header : {
+                Text("Temperature in \(temperatureUnit) is:")
+            }
+        }
     }
 }
 
