a90e896b6433b32dd621cf56ef79f18bda89a7ef
Gabriel Henrique <gabhenrique0@gmail.com>
2022-02-05 21:11:53 -0300

    :sparkles: Feat: add navigation view, title and done button on keyboard

--- UnitsConversor/ContentView.swift
+++ UnitsConversor/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var temperature: Double = 20
     @State private var temperatureUnitEntrancy: UnitTemperature = .celsius
     @State private var temperatureUnitConversion: UnitTemperature = .celsius
+    @FocusState private var temperatureIsFocused: Bool
     private let temperatureUnits: [UnitTemperature] = [.celsius, .fahrenheit, .kelvin]
     
     // MARK: - Computed Properties
@@ -23,40 +24,54 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Form {
-            Section {
-                Picker("Temperature Unit", selection: $temperatureUnitEntrancy) {
-                    ForEach(temperatureUnits, id: \.self) {
-                        Text($0.symbol)
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Temperature Unit", selection: $temperatureUnitEntrancy) {
+                        ForEach(temperatureUnits, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header : {
+                    Text("Conversion Unit")
+                }
+                
+                Section {
+                    TextField("Temperature", value: $temperature, format: .number)
+                        .keyboardType(.numberPad)
+                        .focused($temperatureIsFocused)
+                } header : {
+                    Text("Temperature")
+                }
+                
+                Section {
+                    Picker("Temperature unit to convert", selection: $temperatureUnitConversion) {
+                        ForEach(temperatureUnits, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header : {
+                    Text("Convert temperature in \(temperatureUnitEntrancy.symbol) to:")
+                }
+                
+                Section {
+                    Text(temperatureConverted.description)
+                } header : {
+                    Text("Temperature converted")
+                }
+            }
+            .navigationTitle("Temperature Conversion")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        temperatureIsFocused = false
                     }
                 }
-                .pickerStyle(.segmented)
-            } header : {
-                Text("Unit Conversion")
-            }
-            
-            Section {
-                TextField("Temperature", value: $temperature, format: .number)
-                    .keyboardType(.numberPad)
-            } header : {
-                Text("Temperature")
-            }
-            
-            Section {
-                Picker("Temperature unit to convert", selection: $temperatureUnitConversion) {
-                    ForEach(temperatureUnits, id: \.self) {
-                        Text($0.symbol)
-                    }
-                }
-                .pickerStyle(.segmented)
-            } header : {
-                Text("Convert temperature in \(temperatureUnitEntrancy.symbol) to:")
-            }
-            
-            Section {
-                Text(temperatureConverted.description)
-            } header : {
-                Text("Temperature converted")
             }
         }
     }
